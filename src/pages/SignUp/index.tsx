dcb3c82214f0cc67d8c66898fa17b594213ba22d
NicolasSuzigan <nicolas_suzigan@hotmail.com>
2020-06-19 11:35:52 -0300

    Criação do tooltip, tratamento de errors de formulario

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useRef} from 'react';
 import {FiMail, FiLock, FiArrowLeft, FiUser} from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import {Form} from '@unform/web';
 import * as Yup from 'yup';
 
@@ -7,12 +8,18 @@ import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationError'
+
 
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit= useCallback(async (data: object) => {
     try{
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string().required('E-mail obrigatorio').email('Digite um e-mail valido'),
@@ -23,7 +30,9 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     }catch (err) {
-      console.log(err);
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -33,7 +42,7 @@ const SignUp: React.FC = () => {
     <Content>
       <img src= {logoImg} alt ="Gobarber"/>
 
-      <Form onSubmit={handleSubmit}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <h1>Faça seu cadastro</h1>
         <Input name="name" icon={FiUser} placeholder="Nome"/>
         <Input name="email" icon={FiMail} placeholder="E-mail"/>
