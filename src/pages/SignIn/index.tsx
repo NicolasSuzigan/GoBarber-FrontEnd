f945253cbfe6bd9cb6300831e698adda642b4812
NicolasSuzigan <nicolas_suzigan@hotmail.com>
2020-06-20 12:30:13 -0300

    Inicio das authenticações de usuario

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import {FiLogIn, FiMail, FiLock} from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import { AuthContext } from '../../context/AuthContext';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -7,31 +11,69 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
+import { FormHandles } from '@unform/core';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src= {logoImg} alt ="Gobarber"/>
+import getValidationErrors from '../../utils/getValidationError';
 
-      <form>
-        <h1>Faça seu logon</h1>
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
-        <Input name="email" icon={FiMail} placeholder="E-mail"/>
-        <Input name="password" icon={FiLock} type="password"placeholder="Senha"/>
 
-        <Button type="submit">Entrar</Button>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+  const {signIn} = useContext(AuthContext);
 
-      <a href="oi">
-        <FiLogIn />
-        Criar conta
-        </a>
+  const handleSubmit= useCallback(async (data: SignInFormData) => {
+    try{
+      formRef.current?.setErrors({});
 
-    </Content>
-    <Background />
-  </Container>
-);
+      const schema = Yup.object().shape({
+        email: Yup.string().required('E-mail obrigatorio').email('Digite um e-mail valido'),
+        password: Yup.string().required('Senha Obrigatoria'),
+      });
 
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      signIn({
+        email: data.email,
+        password: data.password,
+      });
+    }catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, [signIn]);
+
+  return(
+    <Container>
+      <Content>
+        <img src= {logoImg} alt ="Gobarber"/>
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail"/>
+          <Input name="password" icon={FiLock} type="password"placeholder="Senha"/>
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="oi">
+          <FiLogIn />
+          Criar conta
+          </a>
+
+      </Content>
+      <Background />
+    </Container>
+  );
+}
 export default SignIn;
